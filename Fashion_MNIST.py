3cad10108dc67894a7a798741b29425b6452a4e6
20n3091 <yan.li.3m@stu.hosei.ac.jp>
2023-01-12 22:33:11 +0800

    first commit

---
+++ Fashion_MNIST.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+import pandas as pd
+import streamlit as st
+from PIL import Image
+from sklearn.preprocessing import StandardScaler
+import keras
+
+model = keras.models.load_model('models/fashion_model.h5')
+Indicator_mnist = pd.read_csv('Fashion_MNIST/Indicator_mnist.csv')
+
+st.title('Model weight:')
+st.image('Fashion_MNIST/fashion_model.png', channels="BGR")
+
+st.title('Model evaluation indicators:')
+st.write('Accuracy')
+st.line_chart(Indicator_mnist[["acc", "val_acc"]])
+st.write('loss')
+st.line_chart(Indicator_mnist[["loss", "val_loss"]])
+
+# Upload pictures and display
+uploaded_file = st.file_uploader("Upload a picture")
+
+if uploaded_file is not None:
+    # Convert the incoming file to Opencv format
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    opencv_image = cv2.imdecode(file_bytes, 1)
+    # show image
+    st.image(opencv_image, channels="BGR")
+    scaler = StandardScaler()
+    cv2.imwrite('test.jpg', opencv_image)
+    im = Image.open('test.jpg')
+    im = im.resize((28, 28), Image.ANTIALIAS)
+    L = im.convert('L')
+    im2 = np.array(L)
+    im2 = np.array([255 - im2])
+    im3 = scaler.fit_transform(im2.astype(np.float32).reshape(-1, 1)).reshape(-1, 28, 28)
+    y = model.predict(im3)
+    class_names = ['T-shirt', 'trousers', 'pullover', 'dress', 'coat',
+                   'Sandals', 'Shirts', 'Sneakers', 'Bags', 'Boots']
+    st.write('Weighted score：')
+    st.dataframe(pd.DataFrame(y[0], index=class_names, columns=["weighted_score"]))
+    st.write('The predicted picture is：')
+    st.title(class_names[np.argmax(y[0])])
